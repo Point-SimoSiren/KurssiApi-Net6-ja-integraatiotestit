b88c6c335685e7818c187a850593739d0d642337
Simo Sirén <simo.siren1@careeria.fi>
2023-03-13 09:46:37 +0200

    Add project files.

---
+++ KurssiApiNet6/Controllers/KurssitController.cs
@@ -0,0 +1,100 @@
+﻿using KurssiApiNet6.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using KurssiApiNet6.Models;
+
+namespace KurssiApiNet6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KurssitController : ControllerBase
+    {
+        private KurssiDBContext db = new KurssiDBContext();
+
+        // Hae kaikki kurssit
+        [HttpGet]
+        [Route("")]
+        public List<Kurssit> HaeKurssit()
+        {
+            try
+            {
+                List<Kurssit> kurssit = db.Kurssit.ToList();
+                return kurssit;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+        }
+
+
+        // Haku id:llä
+        [HttpGet]
+        [Route("{id}")]
+        public Kurssit GetOneById(int id)
+        {
+            try
+            {
+                Kurssit kurssi = db.Kurssit.Find(id);
+                return kurssi;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+        }
+
+
+        // Uuden luonti
+        [HttpPost]
+        [Route("")]
+        public ActionResult LuoKurssi([FromBody] Kurssit kurssi)
+        {
+            try
+            {
+                db.Kurssit.Add(kurssi);
+                db.SaveChanges();
+                return Ok(kurssi.KurssiId);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+            finally
+            {
+                db.Dispose();
+            }
+        }
+
+
+        //Poisto Id:llä
+        [HttpDelete]
+        [Route("{id}")]
+        public ActionResult PoistaKurssi(int id)
+        {
+            try
+            {
+                Kurssit kurssi = db.Kurssit.Find(id);
+                if (kurssi != null)
+                {
+                    db.Kurssit.Remove(kurssi);
+                    db.SaveChanges();
+                    return Ok("Kurssi id:llä " + id + " poistettiin");
+                }
+                else
+                {
+                    return NotFound("Kurssia id:llä" + id + " ei löydy");
+                }
+            }
+            catch
+            {
+                return BadRequest();
+            }
+            finally
+            {
+                db.Dispose();
+            }
+        }
+
+    }
+}
